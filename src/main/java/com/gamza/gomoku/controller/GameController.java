4e4cbaab8aadc4f35f43b34e882afb1cd6faddd2
jihy30n <jihyeon546@gmail.com>
2023-12-16 21:55:22 +0900

    UnityWebRequest

--- src/main/java/com/gamza/gomoku/controller/GameController.java
+++ src/main/java/com/gamza/gomoku/controller/GameController.java
@@ -1,7 +1,6 @@
 package com.gamza.gomoku.controller;
 
 import com.gamza.gomoku.dto.user.LoginRequestDto;
-import com.gamza.gomoku.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +9,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/game")
 @RequiredArgsConstructor
 public class GameController {
-    //private final GameService gameservice;
 
-    @PostMapping("/test")
+    @GetMapping("/test")
     public ResponseEntity<String> receiveJson(@RequestBody LoginRequestDto requestDto) {
         String id = requestDto.getUserEmail();
         String password = requestDto.getPassword();
         System.out.println("Received JSON: " + id + password);
         return ResponseEntity.ok("JSON received successfully");
     }
-
-
 
 }
